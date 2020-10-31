0a23d65c0ad03398425e6e2d379171382a881566
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-10-31 20:27:47 +0200

    add navigation Options

--- ToDo_app/routes/homeStack.js
+++ ToDo_app/routes/homeStack.js
@@ -1,19 +1,32 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
- 
 import Home from '../screens/home';
-import ReviewDetails from '../screens/reviewDetails'
- 
+import ReviewDetails from '../screens/reviewDetails';
+
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: 'GameZone',
+      //headerStyle: { backgroundColor: '#eee' }
+    }
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: 'Review Details',
+      //headerStyle: { backgroundColor: '#eee' }
+    }
   },
 };
 
 // home stack navigator screens
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerTitleAlign: 'center',
+    headerStyle: { backgroundColor: '#B7B7B7', height: 60 },
+  }
+});
 
 export default createAppContainer(HomeStack);
