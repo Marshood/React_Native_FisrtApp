c678b878c156aa5e494320c2b09d88f7789b31df
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-11-01 14:32:33 +0200

    adding Modal on home page

--- ToDo_app/screens/home.js
+++ ToDo_app/screens/home.js
@@ -1,16 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card'
+import { MaterialIcons } from '@expo/vector-icons';
+
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    { title: 'Not So "Final" Fantasy', rating: 2, body: 'lorem ipsum', key: '4' },
+    { title: 'Not So "Final" Fantasy', rating: 1, body: 'lorem ipsum', key: '6' },
+    { title: 'Not So "Final" Fantasy', rating: 4, body: 'lorem ipsum', key: '5' },
   ]);
 
+  const [modalOpen, setModalOpen] = useState(false)
   return (
     <View style={globalStyles.container}>
+
+      <Modal visible={modalOpen} animationType='slide'>
+
+        <View style={styless.modalContent}>
+          <MaterialIcons
+            name='close'
+            size={24}
+            style={{ ...styless.modalToggle, ...styless.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>
+            Hello
+          </Text>
+        </View>
+
+      </Modal>
+
+      <MaterialIcons
+        name='add'
+        size={24}
+        style={styless.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
+
       <FlatList data={reviews} renderItem={({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
           <Card>
@@ -21,3 +51,24 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styless = StyleSheet.create({
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#B7B7B7',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  modalContent: {
+    flex: 1,
+
+  }
+
+})
--- ToDo_app/screens/reviewDetails.js
+++ ToDo_app/screens/reviewDetails.js
@@ -4,6 +4,8 @@ import { globalStyles,images } from '../styles/global';
 import Card from '../shared/card';
  export default function ReviewDetails({ navigation }) {
    const ratingKey=navigation.getParam('rating');
+   console.log("ratingKey",ratingKey)
+   
   return (
     <View style={globalStyles.container}>
       <Card>
@@ -25,6 +27,9 @@ const styles=StyleSheet.create({
     paddingTop:16,
     marginTop:16,
     borderTopWidth:1,
-    borderTopColor:'#B7B7B7'
-  }
+    borderTopColor:'#B7B7B7',
+    resizeMode: 'cover',
+    
+  },
+  
 });
